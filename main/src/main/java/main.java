9ef5d5d2f6cd6f9cdbde78f300d4ef502edc49d0
FeridMehtiyev <feridmehdiyevpersonal@gmail.com>
2025-04-30 12:28:38 +0400

    First Comment
    only an empty main file and ObjectMapper

---
+++ main/src/main/java/main.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        ObjectMapper mapper = new ObjectMapper();
+    }
+}
