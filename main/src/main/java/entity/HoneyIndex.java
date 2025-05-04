04b28b194fe26053a063cfd5b95bb1f530589344
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-05-04 18:07:23 +0400

    Create HoneyIndex.java

---
+++ main/src/main/java/entity/HoneyIndex.java
@@ -0,0 +1,23 @@
+
+package entity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class HoneyIndex {
+    private int index;
+    private int UID;
+
+    public int getIndex() {return index;}
+    public int getUID() {return UID;}
+
+    public HoneyIndex(int UID, int index) {this.UID = UID;this.index = index;}
+    public HoneyIndex() {};
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || obj.getClass()!=HoneyIndex.class) return false;
+        HoneyIndex other = (HoneyIndex) obj;
+        return this.UID == other.UID && this.index == other.index;
+    }
+}
