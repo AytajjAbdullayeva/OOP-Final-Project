c7ef99106860e8e807c4bc8de7f30e877609883a
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-05-04 18:27:20 +0400

    Update and rename main/src/test/java/UserServiceTest.java to main/src/test/java/service/UserServiceTest.java

--- main/src/test/java/service/UserServiceTest.java
+++ main/src/test/java/service/UserServiceTest.java
@@ -1,4 +1,9 @@
+package service;
+
+import DAO.HoneyIndexDAO;
+import DAO.UserDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
