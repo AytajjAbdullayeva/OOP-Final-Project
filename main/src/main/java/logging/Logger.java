e5044982404d945905497ab68fae3821af634197
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-05-04 18:13:04 +0400

    Update and rename 1.txt to Logger.java

---
+++ main/src/main/java/logging/Logger.java
@@ -0,0 +1,31 @@
+package Logging;
+
+import java.nio.file.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class Logger
+{
+    public static boolean DebugLog(String message)
+    {
+        Path path = Paths.get("logs/");
+        Path dlog = path.resolve("log.txt");
+
+        LocalDateTime now = LocalDateTime.now(); // adding time to our log message
+        message=now.toString()+"-->"+message+"\n";
+
+
+        try {
+            Files.createDirectories(path); // will create our logs/directory if needed
+
+            if(!Files.exists(dlog)) Files.write(dlog, message.getBytes()); // if file doesn't exists it will create
+            else Files.write(dlog, message.getBytes(), StandardOpenOption.APPEND);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
