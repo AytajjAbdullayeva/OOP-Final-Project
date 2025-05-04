c8122cee51d2151421a0baf8e09557eaa80f48b8
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-05-04 18:22:24 +0400

    Delete main/src/main/java/FlightNotFoundException.java

--- main/src/main/java/FlightNotFoundException.java
+++
@@ -1,5 +0,0 @@
-public class FlightNotFoundException extends RuntimeException {
-    public FlightNotFoundException(String flightId) {
-        super("Flight with ID " + flightId + " not found");
-    }
-}
