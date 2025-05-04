af407e6139f8874bde0c93de87dfc7376e691b8a
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-05-04 18:09:37 +0400

    Create SessionManager.java

---
+++ main/src/main/java/entity/SessionManager.java
@@ -0,0 +1,58 @@
+package entity;
+
+import logging.Logger;
+
+import javax.swing.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class SessionManager {
+    private static ArrayList<Session> sessions = new ArrayList<Session>();
+
+    public static void addSession(String username)
+    {
+        Session session = new Session(username);
+        sessions.add(session);
+        Logger.DebugLog("Session created");
+    }
+    public static boolean removeSession(String username)
+    {
+        int index=-1;
+        for(int i=0; i<sessions.size(); i++)
+        {
+            if(sessions.get(i).getUserName()==username)
+            {
+                index=i;
+                break;
+            }
+        }
+        if(index==-1)
+        {
+            Logger.DebugLog("Session not found for username: "+username);
+            return false;
+        }
+        int sessionID=sessions.get(index).getSessionID();
+        sessions.remove(index);
+        Logger.DebugLog("Session removed with sessionid: "+Integer.toString(sessionID));
+        return true;
+    }
+
+    public static LocalDateTime StartTime(String username)
+    {
+        int index=-1;
+        for(int i=0; i<sessions.size(); i++)
+        {
+            if(sessions.get(i).getUserName()==username)
+            {
+                index=i;
+                break;
+            }
+        }
+        if(index==-1)
+        {
+            Logger.DebugLog("Session not found for username: "+username);
+            return null;
+        }
+        return sessions.get(index).getCreationTime();
+    }
+}
